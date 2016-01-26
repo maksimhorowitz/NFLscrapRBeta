200605b642eeed719528a9b4a043e0f55c0818b9
maksimhorowitz <mahorowi@andrew.cmu.edu>
2016-01-26 01:40:00 -0500

    Updated Playergame function to include teams. Moved unlist into playergame function from playergameseason function. Started writing player season functions.

--- NFLPlayerGameFunctions.R
+++ NFLPlayerGameFunctions.R
@@ -7,10 +7,12 @@ library(RCurl)
 library(stringr)
 library(lubridate)
 
+source("ReadingNFLJSONData.R")
+
 
 PlayerGame.Function <- function(URLString) {
   # This function outputs a single dataframe containing all rushing, passing,
-  # and receiving statistics for each player in each game.  Each player is 
+  # and receiving statistics for each player in a single  game.  Each player is 
   # assigned one line associated wih their statisitcs
   # Args:
   #      URLString - a string containing the URL of the relevant JSON data
@@ -25,35 +27,66 @@ PlayerGame.Function <- function(URLString) {
   # Here we build the dataframes for the rushing, passing, receving, and 
   # fumbling stats
   
-  dfpass <- rbind(data.frame(t(sapply(nfl.json[[1]][[1]]$stats$passing, c))),
-                  data.frame(t(sapply(nfl.json[[1]][[2]]$stats$passing, c))))
-  dfrush <- rbind(data.frame(t(sapply(nfl.json[[1]][[1]]$stats$rushing, c))),
-                  data.frame(t(sapply(nfl.json[[1]][[2]]$stats$rushing, c))))
-  dfrec <- rbind(data.frame(t(sapply(nfl.json[[1]][[1]]$stats$receiving, c))),
-                 data.frame(t(sapply(nfl.json[[1]][[2]]$stats$receiving, c))))
-  #dffumb <- rbind(data.frame(t(sapply(nfl.json[[1]][[1]]$stats$fumbles, c))),
-  #              data.frame(t(sapply(nfl.json[[1]][[2]]$stats$fumbles, c))))
-  #colnames(dffumb) <- c("name", "totalfumbs", "recfumbs", "totalrecfumbs",
-  #                     "fumbyds", "fumbslost")
+  dfpass <- rbind(data.frame(Team = nfl.json[[1]][[1]]$abbr,
+                             t(sapply(nfl.json[[1]][[1]]$stats$passing, c))),
+                  data.frame(Team = nfl.json[[1]][[2]]$abbr,
+                             t(sapply(nfl.json[[1]][[2]]$stats$passing, c))))
+  dfrush <- rbind(data.frame(Team = nfl.json[[1]][[1]]$abbr,
+                             t(sapply(nfl.json[[1]][[1]]$stats$rushing, c))),
+                  data.frame(Team = nfl.json[[1]][[2]]$abbr,
+                             t(sapply(nfl.json[[1]][[2]]$stats$rushing, c))))
+  dfrec <- rbind(data.frame(Team = nfl.json[[1]][[1]]$abbr,
+                            t(sapply(nfl.json[[1]][[1]]$stats$receiving, c))),
+                 data.frame(Team = nfl.json[[1]][[2]]$abbr,
+                            t(sapply(nfl.json[[1]][[2]]$stats$receiving, c))))
+  dffumb <- rbind(data.frame(Team = nfl.json[[1]][[1]]$abbr,
+                             t(sapply(nfl.json[[1]][[1]]$stats$fumbles, c))),
+                data.frame(Team = nfl.json[[1]][[2]]$abbr,
+                           t(sapply(nfl.json[[1]][[2]]$stats$fumbles, c))))
+  
+  if (nrow(dffumb) == 0) {
+    dffumb <- NULL
+    
+    # Initialize a new variable with the player IDs
+    dfpass$playerID <- rownames(dfpass)
+    dfrush$playerID <- rownames(dfrush)
+    dfrec$playerID <- rownames(dfrec)
+    
+    # This stage is where we merge all the dataframes together so each player 
+    # has one line
+    final.df <- Reduce(function(x, y) 
+    {merge(x, y, by = c("Team", "playerID", "name"),all=TRUE)},
+    list(dfpass, dfrush, dfrec))
+    
+    # Adding Fumble columns with 0's due to no occurance of fumbles in game
+    final.df$totalfumbs <- 0
+    final.df$recfumbs <- 0
+    final.df$totalrecfumbs <- 0
+    final.df$fumbyds <- 0
+    final.df$fumbslost <- 0
+  }
+  
+  else {
+  colnames(dffumb) <- c("Team", "name", "totalfumbs", "recfumbs", "totalrecfumbs",
+                       "fumbyds", "fumbslost")
   # Initialize a new variable with the player IDs
   dfpass$playerID <- rownames(dfpass)
   dfrush$playerID <- rownames(dfrush)
   dfrec$playerID <- rownames(dfrec)
-  #dffumb$playerID <- rownames(dffumb)
-  
+  dffumb$playerID <- rownames(dffumb)
+
   # This stage is where we merge all the dataframes together so each player 
   # has one line
   final.df <- Reduce(function(x, y) 
-  {merge(x, y, by = c("playerID", "name"),all=TRUE)},
-  list(dfpass, dfrush, dfrec))
+  {merge(x, y, by = c("Team","playerID", "name"),all=TRUE)},
+  list(dfpass, dfrush, dfrec, dffumb))
+  }
   
-  #if (!is.null(dffumb)) {
-  # final.df <- merge(final.df, dffumb, by = c("playerID", "name"),
-  #                sort = FALSE, all = TRUE)
-  #}
-  
-  final.df <- sapply(final.df, function(x) ifelse(x == "NULL", NA, x))
+  final.df <- data.frame(final.df[,1],
+                    sapply(final.df[,-1], 
+                           function(x) ifelse((x == "NULL" | is.na(x)), 0, x)))
   rownames(final.df) <- NULL
+  
   
   #   GameID
   gameID <- str_extract(URLString, pattern = "[0-9]{10}")
@@ -68,51 +101,75 @@ PlayerGame.Function <- function(URLString) {
   
   # Output dataframe which has the gameID, date of game, and the player info
   # and statistics 
-  data.frame(gameID, date, final.df)
+  final.df2 <- data.frame(gameID, date, final.df)
+  
+  
+  # Unlist the listed variables in order to return the output dataframe in a 
+  # friendlier format
+  
+  final.df2$playerID <- unlist(final.df2$playerID)
+  final.df2$name <- unlist(final.df2$name)
+  final.df2$Team.x <- unlist(final.df2$Team.x)
+  final.df2$att.x <- unlist(final.df2$att.x)
+  final.df2$cmp <- unlist(final.df2$cmp)
+  final.df2$yds.x <- unlist(final.df2$yds.x)
+  final.df2$tds.x <- unlist(final.df2$tds.x)
+  final.df2$ints <- unlist(final.df2$ints)
+  final.df2$twopta.x <- unlist(final.df2$twopta.x)
+  final.df2$twoptm.x <- unlist(final.df2$twoptm.x)
+  final.df2$att.y <- unlist(final.df2$att.y)
+  final.df2$yds.y <- unlist(final.df2$yds.y)
+  final.df2$tds.y <- unlist(final.df2$tds.y)
+  final.df2$lng.x <- unlist(final.df2$lng.x)
+  final.df2$lngtd.x <- unlist(final.df2$lngtd.x)
+  final.df2$twopta.y <- unlist(final.df2$twopta.y)
+  final.df2$twoptm.y <- unlist(final.df2$twoptm.y)
+  final.df2$rec <- unlist(final.df2$rec)
+  final.df2$yds <- unlist(final.df2$yds)
+  final.df2$tds <- unlist(final.df2$tds)
+  final.df2$lng.y <- unlist(final.df2$lng.y)
+  final.df2$lngtd.y <- unlist(final.df2$lngtd.y)
+  final.df2$twopta <- unlist(final.df2$twopta)
+  final.df2$twoptm <- unlist(final.df2$twoptm)
+  final.df2$totalfumbs <- unlist(final.df2$totalfumbs)
+  final.df2$recfumbs <- unlist(final.df2$recfumbs)
+  final.df2$totalrecfumbs <- unlist(final.df2$totalrecfumbs)
+  final.df2$fumbyds <- unlist(final.df2$fumbyds)
+  final.df2$fumbslost <- unlist(final.df2$fumbslost)
+  
+  final.df2
 }
 
-PlayerGame.Function(nfl.data.url2)
+nfl.data.urltest <- "http://www.nfl.com/liveupdate/game-center/2013090800/2013090800_gtd.json"
+
+PlayerGameTest <- PlayerGame.Function(nfl.data.urltest)
+
+
 
 SeasonPlayerGame <- function(Year) {
-  
+  # This function outputs a single dataframe containing all rushing, passing,
+  # receiving, and fumble statistics for each player in each game.  Each player 
+  # is assigned one line associated wih their statisitcs per game
+  # Args:
+  #      Year - a numeric 4 digit number specifying a year 
+  # Returns: 
+  #      A dataframe containing the rushing, receiving, passing, and fumble 
+  #      statistics for each player that recorded such a statistic in everygame 
+  #      in a season
+  #
   gameIDS <- Extracting_NFL_GameIDs(Year)
   gameURLs <- sapply(gameIDS, Proper.PBP.URL.Formatting)
   playergameseason.unformatted <- lapply(gameURLs, FUN = PlayerGame.Function)
   
+  # Rowbinding all the games from the specified season
+  
   playergameseason <- do.call(rbind, playergameseason.unformatted)
   
-  playergameseason$playerID <- unlist(playergameseason$playerID)
-  playergameseason$name <- unlist(playergameseason$name)
-  playergameseason$att.x <- unlist(playergameseason$att.x)
-  playergameseason$cmp <- unlist(playergameseason$cmp)
-  playergameseason$yds.x <- unlist(playergameseason$yds.x)
-  playergameseason$tds.x <- unlist(playergameseason$tds.x)
-  playergameseason$ints <- unlist(playergameseason$ints)
-  playergameseason$twopta.x <- unlist(playergameseason$twopta.x)
-  playergameseason$twoptm.x <- unlist(playergameseason$twoptm.x)
-  playergameseason$att.y <- unlist(playergameseason$att.y)
-  playergameseason$yds.y <- unlist(playergameseason$yds.y)
-  playergameseason$tds.y <- unlist(playergameseason$tds.y)
-  playergameseason$lng.x <- unlist(playergameseason$lng.x)
-  playergameseason$lngtd.x <- unlist(playergameseason$lngtd.x)
-  playergameseason$twopta.y <- unlist(playergameseason$twopta.y)
-  playergameseason$twoptm.y <- unlist(playergameseason$twoptm.y)
-  playergameseason$rec <- unlist(playergameseason$rec)
-  playergameseason$yds <- unlist(playergameseason$yds)
-  playergameseason$tds <- unlist(playergameseason$tds)
-  playergameseason$lng.y <- unlist(playergameseason$lng.y)
-  playergameseason$lngtd.y <- unlist(playergameseason$lngtd.y)
-  playergameseason$twopta <- unlist(playergameseason$twopta)
-  playergameseason$twoptm <- unlist(playergameseason$twoptm)
-  playergameseason$totalfumbs <- unlist(playergameseason$totalfumbs)
-  playergameseason$recfumbs <- unlist(playergameseason$recfumbs)
-  playergameseason$totalrecfumbs <- unlist(playergameseason$totalrecfumbs)
-  playergameseason$fumbyds <- unlist(playergameseason$fumbyds)
-  playergameseason$fumbslost <- unlist(playergameseason$fumbslost)
-  
+  # Final output dataframe
   playergameseason
 }
 
+sapply(PlayerGame2010, class)
 
 # Here we collect the playergame data for all games from 2010-2014
 
@@ -121,6 +178,23 @@ PlayerGame2011 <- SeasonPlayerGame(2011)
 PlayerGame2012 <- SeasonPlayerGame(2012)
 PlayerGame2013 <- SeasonPlayerGame(2013)
 PlayerGame2014 <- SeasonPlayerGame(2014)
+
+### Player Season Function ###
+
+PlayerSeason.Function <- function(Year) {
+  
+  PlayerData.Year <- SeasonPlayerGame(Year)
+  
+  TestSeasonAgg <- ddply(PlayerData.Year[,-c(1,2)], .(playerID, name), numcolwise(sum))
+  
+}
+
+PlayerSeason.Function()
+
+TestSeasonAgg <- ddply(PlayerGame2010[,-c(1,2)], .(playerID, name), numcolwise(sum))
+
+
+
 
 # Combining them into one dataframe
 
